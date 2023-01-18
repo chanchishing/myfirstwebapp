ea94512e83b25e8c820d0abf9278953e6c19172e
chanchishing <chishing@netvigator.com>
2023-01-18 16:50:40 +0800

    Add view using jsp

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -11,6 +11,28 @@ public class SayHelloController {
     @ResponseBody
     public String sayHello(){
         return "Hello! What are you learning today?";
-
     }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml(){
+        StringBuffer sb =new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>My first HTML Page - Changed</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first html page with body - Changed");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
+    }
+
+    @RequestMapping("say-hello-jsp")
+    public String sayHellojsp(){
+        return "sayHello";
+    }
+
+
 }
