fbcd336cf3128ed17fab6fdc1520bc5f9f72f445
shing <chishing@netvigator.com>
2023-01-30 21:11:20 +0800

    Add h2 DB to project and by-pass spring security default of reject  h2-console access (disable CSRF and Frame Option)

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -38,5 +41,24 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        //All URLs are protected
+        httpSecurity.authorizeHttpRequests(
+          auth -> auth.anyRequest().authenticated()
+        );
+
+        //A login form is show for unauthorized reuests
+        httpSecurity.formLogin(Customizer.withDefaults());
+
+        //for h2-console, needed to
+        //(a) disable CSRF
+        //(b) all Frame option
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+
+        return httpSecurity.build();
     }
 }
