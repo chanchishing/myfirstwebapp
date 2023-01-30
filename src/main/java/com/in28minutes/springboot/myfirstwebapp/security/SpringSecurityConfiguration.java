2d64e78896c15c3f26cf6c34567342149024ade4
chanchishing <chishing@netvigator.com>
2023-01-30 17:25:20 +0800

    modify security configuration bean to add additional users

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -17,14 +17,22 @@ public class SpringSecurityConfiguration {
     public InMemoryUserDetailsManager createUserDetailsManager(){
         Function<String, String> passwordEncoder=input->passwordEncoder().encode(input);
 
+        UserDetails userDetails1 = createNewUser("in28minutes", "dummy");
+        UserDetails userDetails2 = createNewUser("ranga", "dummy");
+
+        return new InMemoryUserDetailsManager(userDetails1,userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
+        Function<String, String> passwordEncoder=input->passwordEncoder().encode(input);
+
         UserDetails userDetails=User.builder()
                 .passwordEncoder(passwordEncoder)
-                .username("in28minutes")
-                .password("dummy")
+                .username(username)
+                .password(password)
                 .roles("USER","ADMIN")
                 .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
