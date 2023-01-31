961e2436574a0ee09250e0872b35ec9970a17465
chanchishing <chishing@netvigator.com>
2023-01-31 13:37:02 +0800

    retrieves todos from H2 using jpa and display them

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoControllerJPA.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoControllerJPA.java
@@ -13,19 +13,23 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import java.time.LocalDate;
 import java.util.List;
 
-//@Controller
+@Controller
 @SessionAttributes("name")
-public class TodoController {
+public class TodoControllerJPA {
 
     private TodoService todoService;
 
-    public TodoController(TodoService todoService) {
+    private TodoRepository todoRepository;
+
+    public TodoControllerJPA(TodoService todoService, TodoRepository todoRepository) {
         this.todoService = todoService;
+        this.todoRepository = todoRepository;
     }
 
     @RequestMapping("list-todos")
     public String listAllTodos(ModelMap model) {
-        List<Todo> todos= todoService.findByUsername(getLoggedInUsername(model));
+        //List<Todo> todos= todoService.findByUsername(getLoggedInUsername(model));
+        List<Todo> todos= todoRepository.findByUsername(getLoggedInUsername(model));
         model.addAttribute("todos",todos);
         return "listTodos";
     }
